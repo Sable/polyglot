68c9982d2c7dc2a0f9e8e4e562ec83adb18288a4
Jennifer Lhoták <jlhotak@sable.mcgill.ca>
2006-04-19 01:57:28 +0000

    - adding missing files to svn

---
+++ src/polyglot/ext/jl5/visit/GenericCastInsertionVisitor.java
@@ -0,0 +1,41 @@
+/* Copyright (C) 2006 Jennifer Lhotak
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+package polyglot.ext.jl5.visit;
+
+import polyglot.ast.*;
+import polyglot.types.*;
+import polyglot.util.*;
+import polyglot.frontend.Job;
+import polyglot.types.Package;
+import polyglot.visit.*;
+
+/** Visitor which performs type checking on the AST. */
+public class GenericCastInsertionVisitor extends ContextVisitor
+{
+    public GenericCastInsertionVisitor(Job job, TypeSystem ts, NodeFactory nf) {
+	    super(job, ts, nf);
+    }
+
+    protected Node leaveCall(Node old, Node n, NodeVisitor v) throws SemanticException {
+        if (n instanceof GenericCastInsertionVisit){
+	        return ((GenericCastInsertionVisit)n).genCastIns((GenericCastInsertionVisitor) v);
+        }
+	    return n;
+    }
+}
